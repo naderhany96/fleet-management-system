5522aec511899f0128e1153da23a032a34bf93ac
naderhany.fintech <naderhany.fintech@gmail.com>
2022-11-18 17:52:00 +0200

    Prepare the project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test',function(){
+    return "Hiiiiii";
+});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
