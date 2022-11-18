fd4b057c5ea5d41b10f44350f36bd427b4731767
naderhany.fintech <naderhany.fintech@gmail.com>
2022-11-18 15:12:55 +0200

    Prepare the project

---
+++ README.md
@@ -0,0 +1,25 @@
+# How To Install Project
+
+### Clone Repository
+
+```php
+git clone https://github.com/naderhany96/fleet-management-system.git
+```
+
+### Install
+
+```php
+composer install
+```
+
+### migration
+
+```php
+php artisan migrate 
+
+```
+
+# Make a database with name 'fleet_management_system' then import the SQL file 
+
+fleet-management-system/database/fleet_management_system.sql
+
---
+++ database/migrations/2022_11_18_104922_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure_place');
+            $table->string('arrival_place');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+};
